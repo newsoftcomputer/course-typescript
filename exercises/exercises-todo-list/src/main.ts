76420a9cc6a40e43ef3f62f57dfd91d9f921fbd9
Andres Giraldo <andresganc@gmail.com>
2023-03-20 10:25:43 -0500

    Mod: Exercises Todo List

--- exercises/exercises-todo-list/src/main.ts
+++ exercises/exercises-todo-list/src/main.ts
@@ -1,9 +1,13 @@
-import './style.css'
+
+// Global Styles
+import './assets/css/global-styles.css'
+
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -17,7 +21,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
-  </div>
+  </div> -->
+  
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
