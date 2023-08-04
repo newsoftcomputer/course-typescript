dbc8bfc5ec8ce4dc9cbdc6b2e2bc0a719510507b
Andres Giraldo <andresganc@gmail.com>
2023-08-04 12:02:29 -0500

    Course Basic - Everyday: Union Types

---
+++ course-nc/01-basic/code/06-everyday-types/6.12-union-types/01-union-types/example-01.ts
@@ -0,0 +1,16 @@
+
+let a: Number | String
+a = 5
+a = 'Hola'
+
+
+let b: Number | String | Boolean
+b = true
+b = 6546
+b = 'Andres' 
+
+
+let c: String | 5 | Boolean
+c = 'Marco'
+c = 5
+c = true
---
+++ course-nc/01-basic/code/06-everyday-types/6.12-union-types/01-union-types/example-03.ts
@@ -0,0 +1,7 @@
+
+function printId(id: number | string) {
+    // Error
+    // console.log(id.toUpperCase());
+    // Property 'toUpperCase' does not exist on type 'string | number'.
+    // Property 'toUpperCase' does not exist on type 'number'.
+}
