3250a6d9e4bc88ef4c590f5026b62f54030be730
Andres Giraldo <andresganc@gmail.com>
2023-08-05 12:21:19 -0500

    New: NC Course Basic - Type Manipulation: Generics, Keyof type operator, Typeof type operator, Indexed Acces Types, Conditional Types

--- course-nc/01-basic/code/06-everyday-types/6.12-union-types/02-template-union-types/example-01.ts
+++ course-nc/01-basic/code/06-everyday-types/6.12-union-types/02-template-union-types/example-01.ts
@@ -1,6 +1,51 @@
 
-type ColorHex = `#${string}`
+// UNIONS TYPES
+//  Make variables of different types
 
-type Email = `${string}@${string}.${string}`
-        
-type Hexadecimal = `${string}-${string}-${string}-${string}-${string}`
+// Example 1
+let value1: number | string
+let value2: number | boolean
+
+// Example 1
+let value3: string | 0
+let value4: 0 | 1
+
+
+// Example 2
+function convertir(value3: string | number) {
+
+}
+
+convertir('Peluche')
+convertir(17)
+
+
+// Example 3
+type A = {
+    uno: boolean | string
+    dos: boolean | number
+}
+
+type B = {
+    tres: boolean
+}
+
+let value5: A | B = {
+    uno: 'Marco',
+    dos: 5,
+    tres: true
+}
+
+
+// Show props of type string or number
+let value6: string | number
+
+value6 = 5
+
+if (typeof(value5) === 'string') {
+    // Now can show the props of type string
+    value6.trim()
+} else if (typeof(value5) === 'number') {
+    // Now can show the props of type string
+    value6.toString()
+}
