6101a1e50cbded3f983f9c388109726915aa8ebd
Andres Giraldo <andresganc@gmail.com>
2023-08-09 20:21:56 -0500

    New: Course NC - Advanced - Modules: ES Modules Syntax

---
+++ course-nc/02-advanced/code/modules/04-es-module-syntax.ts
@@ -0,0 +1,10 @@
+
+/*
+ES Module Syntax
+A file can declare a main export via export default:
+*/
+
+// @filename: hello.ts
+export default function helloWorld() {
+    console.log("Hello, world!");
+}
